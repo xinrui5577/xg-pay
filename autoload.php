6372cfe74b3c7ce4ddb5daefbda87a5e205a1915
xinyu126 <xinrui5577@sina.com>
2018-12-16 12:25:20 +0800

    1.0.3

--- autoload.php
+++ autoload.php
@@ -2,13 +2,12 @@
 
 function classLoader($class)
 {
-    echo $class;
+   
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     $path = str_replace('Payment' . DIRECTORY_SEPARATOR, '', $path);
 
     $file = __DIR__ . '/src/' . $path . '.php';
-    echo $file;
-    die('gg');
+   
     if (file_exists($file)) {
         require_once $file;
     }
